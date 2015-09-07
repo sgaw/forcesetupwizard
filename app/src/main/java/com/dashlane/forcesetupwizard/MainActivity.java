87eb523881cc7a0655d531b3b156fc278369747a
Shirley Gaw <shirley.gaw@dashlane.com>
2015-09-07 18:30:49 +0200

    Use EDU setup

--- app/src/main/java/com/dashlane/forcesetupwizard/MainActivity.java
+++ app/src/main/java/com/dashlane/forcesetupwizard/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dashlane.forcesetupwizard;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,19 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int i = 0; i < 2; i++) {
-            try {
-                int getIntResult = Settings.System.getInt(getContentResolver(),
-                        Settings.System.SETUP_WIZARD_HAS_RUN);
-                Log.i("MainActivity", "SETUP_WIZARD_HAS_RUN: " + getIntResult);
-            } catch (Settings.SettingNotFoundException e) {
-                e.printStackTrace();
-            }
+        //testSetupWizardHasRun();
 
-            boolean putIntResult = Settings.System.putInt(getContentResolver(),
-                    Settings.System.SETUP_WIZARD_HAS_RUN, 0);
-            Log.i("MainActivity", "putInt: " + putIntResult);
-        }
+        Intent intent = new Intent();
+        intent.setAction("com.android.setupwizard.EDU_PRE_SETUP");
+        intent.setFlags(0x4000000);
+        intent.setComponent(new ComponentName("com.google.android.setupwizard",
+                ".EduInitializeWrapper"));
+        sendBroadcast(intent);
+        Log.e("MainActivity", "Sent intent: " + intent.toString());
     }
 
     @Override
@@ -49,5 +47,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void testSetupWizardHasRun() {
+        for (int i = 0; i < 2; i++) {
+            try {
+                int getIntResult = Settings.System.getInt(getContentResolver(),
+                        Settings.System.SETUP_WIZARD_HAS_RUN);
+                Log.i("MainActivity", "SETUP_WIZARD_HAS_RUN: " + getIntResult);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            boolean putIntResult = Settings.System.putInt(getContentResolver(),
+                    Settings.System.SETUP_WIZARD_HAS_RUN, 0);
+            Log.i("MainActivity", "putInt: " + putIntResult);
+        }
     }
 }
