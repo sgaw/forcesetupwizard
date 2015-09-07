49eecd69e7ac939b3e55fd094c1e55662787b6bb
Shirley Gaw <shirley.gaw@dashlane.com>
2015-09-07 11:43:12 +0200

    Uses Activity.getContentResolver to set SETUP_WIZARD_HAS_RUN, but does not actually force a system reset

---
+++ app/src/main/java/com/dashlane/forcesetupwizard/MainActivity.java
@@ -0,0 +1,53 @@
+package com.dashlane.forcesetupwizard;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 2; i++) {
+            try {
+                int getIntResult = Settings.System.getInt(getContentResolver(),
+                        Settings.System.SETUP_WIZARD_HAS_RUN);
+                Log.i("MainActivity", "SETUP_WIZARD_HAS_RUN: " + getIntResult);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            boolean putIntResult = Settings.System.putInt(getContentResolver(),
+                    Settings.System.SETUP_WIZARD_HAS_RUN, 0);
+            Log.i("MainActivity", "putInt: " + putIntResult);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
